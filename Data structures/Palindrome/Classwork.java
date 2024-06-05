f8751456a426dd57276bdad1647b1ca4cb3be6a0
Owlama <130388463+Owlama@users.noreply.github.com>
2024-06-05 04:09:34 +0100

    Add files via upload

---
+++ Data structures/Palindrome/Classwork.java
@@ -0,0 +1,12 @@
+public class Classwork {
+    public static void main(String[] args) {
+        String text = "text";
+        String reversed = reverseText(text);
+        System.out.println(reversed);
+    }
+
+    public static String reverseText(String text) {
+        return new StringBuilder(text).reverse().toString();
+    }
+}
+
